d9047438a1394f827629f7711417227bed7d27be
jvegav <j.vegav@uniandes.edu.co>
2023-01-30 10:10:04 -0500

    Update

---
+++ Taller0_Olimpicos/src/uniandes/dpoo/taller0/modificacion/Modificacion.java
@@ -0,0 +1,16 @@
+package uniandes.dpoo.taller0.modificacion;
+
+import java.io.IOException;
+
+import uniandes.dpoo.taller0.procesamiento.CalculadoraEstadisticas;
+import uniandes.dpoo.taller0.procesamiento.LoaderOlimpicos;
+
+public class Modificacion {
+	public static void main(String[] args) throws IOException, IOException {
+		System.out.println("Hola, Mundo!");
+		CalculadoraEstadisticas calc = LoaderOlimpicos.cargarArchivo("./data/atletas.csv");
+		System.out.println(calc.paisConMasMedallistas());
+		
+	}
+
+}
